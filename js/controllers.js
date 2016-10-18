baae7844aa656b247e189c7d3f74901e5d72deb0
Matt Donovan <mattxd12@gmail.com>
2016-10-18 15:30:34 -0600

    API GET IS WORKING!!!

--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,20 @@
+var app = angular.module("MovieApp", []);
+
 app.controller('MovieSearchController', function($scope, $http){
-  $scope.$watch('search', function() {
+  $scope.$watch('titleSearch', function() {
     fetch();
   });
 
-$scope.titleSearch = '';
+  $scope.titleSearch = '';
 
-function fetch() {
-$http.get("http://img.omdbapi.com/?t="+ $scope.titleSearch + "&tomatoes=true&r=json&plot=full")
-}).then(function successCallback(response) {
-    $scope.details = response.data;
+  function fetch() {
+  $http.get("https://www.omdbapi.com/?t="+ $scope.titleSearch + "&tomatoes=true&r=json&plot=full")
+  .then(function successCallback(response) {
+    console.log($scope.titleSearch);
+      console.log(response.data);
+      $scope.details = response.data;
   }, function errorCallback(response) {
-    console.log(response);
-  });
+        console.log(response);
+    });
+  }
 })
